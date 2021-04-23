4969723331e953d61a0cf303fbf7af58df031bc4
Warbler14 <firefly11a@naver.com>
2021-04-24 00:37:48 +0900

    commit

--- StudyCppX/main.cpp
+++ StudyCppX/main.cpp
@@ -12,7 +12,8 @@ int main(int argc, const char * argv[]) {
     
     time_t timer = time(NULL);
     struct tm * t = localtime(&timer);
-    printf("유닉스 타임 (Unix Time): %ld 초\n\n", timer);
+    
+    printf("유닉스 타임 (Unix Time): %ld 초\n", timer);
     printf("현재 년: %d\n", t->tm_year + 1900);
     printf("현재 월: %d\n", t->tm_mon + 1);
     printf("현재 일: %d\n", t->tm_mday);
@@ -22,8 +23,6 @@ int main(int argc, const char * argv[]) {
     printf("현재 요일: %d\n", t->tm_wday); // 일=0, 월=1, 화=2, 수=3, 목=4, 금=5, 토=6
     printf("올해 몇 번째 날: %d\n", t->tm_yday); // 1월 1일은 0, 1월 2일은 1
     printf("서머타임 적용 여부: %d\n", t->tm_isdst); // 실시 중이면 양수, 미실시면 0, 실시 정보가 없으면 음수
-        
-    
     
     return 0;
 }
