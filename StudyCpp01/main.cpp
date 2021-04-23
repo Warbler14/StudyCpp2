613c4680c05b2cddb2e23ddd25cd6da43d2d702b
Warbler14 <firefly11a@naver.com>
2021-04-23 23:53:59 +0900

    Initial Commit

---
+++ StudyCpp01/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  StudyCpp01
+//
+//  Created by kook on 2021/04/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
